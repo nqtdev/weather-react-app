3e52061afedfcd4d8ae867b9386a5ebad8bb7a12
nqtdev <nguyenquoctien2000@gmail.com>
2022-09-23 00:09:56 +0700

    them icon - hoan thien call api weather

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -2,12 +2,11 @@ import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 import { geoApiOptions, GEO_API_URL } from "../../api";
 
-const Search = (onSearchChange) => {
+const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
-
   const loadOptions = (inputValue) => {
     return fetch(
-      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix= ${inputValue}`,
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
       geoApiOptions
     )
       .then((response) => response.json())
@@ -20,8 +19,7 @@ const Search = (onSearchChange) => {
             };
           }),
         };
-      })
-      .catch((err) => console.error(err));
+      });
   };
   const handleOnChange = (searchData) => {
     setSearch(searchData);
