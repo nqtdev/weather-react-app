21e77b5287bbcd1dd1946d9962ab085bad780a79
nqtdev <nguyenquoctien2000@gmail.com>
2022-09-22 10:18:02 +0700

    create weather-react-app

---
+++ src/api.js
@@ -0,0 +1,8 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "39a55d3747msh14003f7b9f397a2p1d75efjsn324a8b1f184e",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
